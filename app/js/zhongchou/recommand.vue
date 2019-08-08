22f9aa29f50b546c79638bd60a305540423bc306
chenlei <chenlei@npnets.com>
2019-08-08 09:33:24 +0800

    众筹更新

--- app/js/zhongchou/recommand.vue
+++ app/js/zhongchou/recommand.vue
@@ -3,9 +3,9 @@
     <ul>
       <li v-for="item in recommands" :key="item.title">
         <img :src="item.src" />
-        <div>{{item.title}}</div>
-        <div>{{item.desc}}</div>
-        <div>{{item.price}}</div>
+        <div class="title">{{item.title}}</div>
+        <div class="desc">{{item.desc}}</div>
+        <div class="price">{{item.price}}</div>
       </li>
     </ul>
   </panel>
@@ -24,37 +24,37 @@ export default {
           src: require("../../image/recommand0.jpg"),
           title: "银离子速干致冷抗菌T",
           desc: " 抗菌除臭 ",
-          price: 20
+          price: 19.99
         },
         {
           src: require("../../image/recommand1.jpg"),
-          title: "银离子速干致冷抗菌T",
-          desc: " 抗菌除臭 ",
-          price: 20
+          title: "纯天然五谷香米",
+          desc: " 五谷稻米 ",
+          price: 29.99
         },
         {
           src: require("../../image/recommand2.jpg"),
-          title: "银离子速干致冷抗菌T",
-          desc: " 抗菌除臭 ",
-          price: 20
+          title: "银离子高温杀菌酸奶",
+          desc: " 纯真酸奶 ",
+          price: 39.99
         },
         {
           src: require("../../image/recommand0.jpg"),
           title: "银离子速干致冷抗菌T",
           desc: " 抗菌除臭 ",
-          price: 20
+          price: 19.99
         },
         {
           src: require("../../image/recommand1.jpg"),
-          title: "银离子速干致冷抗菌T",
+          title: "纯天然五谷香米T",
           desc: " 抗菌除臭 ",
-          price: 20
+          price: 49.99
         },
         {
           src: require("../../image/recommand2.jpg"),
-          title: "银离子速干致冷抗菌T",
+          title: "银离子高温杀菌酸奶",
           desc: " 抗菌除臭 ",
-          price: 20
+          price:59.99
         }
       ]
     };
@@ -66,7 +66,8 @@ export default {
 .panel {
   @include panel;
   box-sizing: border-box;
-  height: 800px;
+  height: 34rem;
+  margin-bottom: 4rem;
   ul {
     display: flex;
     flex-wrap: wrap;
@@ -74,7 +75,7 @@ export default {
       width: 50%;
       &:nth-child(even) {
         box-sizing: border-box;
-        padding: 20px;
+        padding:0 20px;
         left: 0;
         img {
           width: 100%;
@@ -83,13 +84,27 @@ export default {
       &:nth-child(odd) {
         box-sizing: border-box;
         right: 0;
-        padding: 20px;
+        padding:0 20px;
         img {
           width: 100%;
         }
       }
     }
   }
+}
+.title{
+  margin-top: 8px;
+    font-size: 28px;
+    font-weight: 800;
+}
+.desc{
+  padding: 10px 0 0 0;
+  color: #666;
+  font-size: 24px;
+}
+.price{
+  padding:  0;
+  color: rgb(245, 47, 52);
 }
 </style>
 
